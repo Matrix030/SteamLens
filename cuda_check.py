88e3b6772977f9c095c23dbe483b60189932177a
Matrix030 <rishigharat@gmail.com>
2025-06-02 11:34:43 -0400

    deleted length check files

--- cuda_check.py
+++
@@ -1,5 +0,0 @@
-import torch
-print(torch.__version__)           # e.g. 2.1.0+cu121
-print(torch.cuda.is_available())   # should print True
-print(torch.cuda.device_count())   # should be ≥1
-print(torch.cuda.get_device_name(0))  # should list your RTX 4080 Super
